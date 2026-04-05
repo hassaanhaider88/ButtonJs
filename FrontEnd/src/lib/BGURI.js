46cc4620cd43e583504e2107b97cc1d04413f14f
hassaanhaider88 <hassaanhaider088@gmail.com>
2026-04-05 08:00:11 +0500

    live backend URL

--- FrontEnd/src/lib/BGURI.js
+++ FrontEnd/src/lib/BGURI.js
@@ -1,4 +1,4 @@
-// const BGURI = "https://buttonjs-backend.onrender.com";
-const BGURI = "http://localhost:3000";
+const BGURI = "https://buttonjs-wa33.onrender.com";
+// const BGURI = "http://localhost:3000";
 
 export default BGURI;
